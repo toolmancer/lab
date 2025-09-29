936960c1edbe60da00fca3d3c7c083de91566843
ToolMancer <202981869+toolmancer@users.noreply.github.com>
2025-09-29 07:58:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("ToolMancer Pro loaded 🚀");
+// Highlight active sidebar link
+const links = document.querySelectorAll(".sidebar ul li a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
